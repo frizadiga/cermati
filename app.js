f1bdc81eca65714930930369ea34b87481adf6f6
frizadiga <frizadiga@gmail.com>
2019-01-27 15:23:42 +0700

    update

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-alert('init');
+window.onscroll = function(ev) {
+  console.log({ 
+    scrollY: window.scrollY,
+    windowHeight: window.innerHeight,
+    offsetHeight: document.body.offsetHeight,
+  })
+  
+  const offset = window.innerHeight;
+
+  const sliderElement = document.getElementById('slider')
+
+  if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
+    console.log('scroll trigger')
+    sliderElement.classList.remove('hidden')
+  }
+
+  // const offsetHeight = document.body.offsetHeight;
+
+  // if ((window.innerHeight + window.scrollY) >= offsetHeight) {
+  //     // alert("you're at the bottom of the page");
+  //     console.log('scroll trigger')
+  // }
+};
