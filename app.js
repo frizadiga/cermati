09c430ab8e2fdbfe818462a32678b7668587dd90
frizadiga <frizadiga@gmail.com>
2019-01-28 14:23:43 +0700

    bulk update

--- app.js
+++ app.js
@@ -4,20 +4,45 @@ const fnScroll = (event) => {
   //   windowHeight: window.innerHeight,
   //   offsetHeight: document.body.offsetHeight,
   // });
+
+  const ms = JSON.parse(localStorage.slider).time;
+  const diff = new Date().getTime() - ms;
+  const mm = Math.floor(diff / 1000 / 60);
+  
+  if (localStorage.slider) {
+    console.log({ ms, mm, diff });
+  }
   
   const offset = window.innerHeight;
   const sliderElement = document.getElementById('slider');
-  const isTriggered = document.body.scrollTop > offset 
-    || document.documentElement.scrollTop > offset
+  const isTriggered = () => {
+    const intervalInMinute = 1;
+    
+    return (
+      mm >= intervalInMinute && (
+        document.body.scrollTop > offset ||
+        document.documentElement.scrollTop > offset
+      )
+    )
+  }
 
-  if (isTriggered) {
-    console.log('scroll trigger');
+  if (isTriggered()) {
+    console.log('scroll trigger', { isTriggered });
     sliderElement.classList.remove('slider--hidden');
   }
 };
 
 const fnToggleShow = (id) => {
   if (!id) return;
+  
+  if (true || !localStorage.slider) {
+    const slider = {
+      everClosed: true,
+      time: new Date().getTime(),
+    }
+
+    localStorage.setItem('slider', JSON.stringify(slider))
+  }
 
   const element = document.getElementById(id);
   element.classList.toggle('slider--hidden');
